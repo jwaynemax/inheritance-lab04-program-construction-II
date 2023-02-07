938fb8f41d000fb08da8cde62f31d225253f1ad8
Justin Maxwell <jm00724@my.westga.edu>
2023-02-07 15:54:59 -0500

    GoWestLumber constructor, getter, and setter added

--- src/edu/westga/cs6312/controller/GoWestLumber.java
+++ src/edu/westga/cs6312/controller/GoWestLumber.java
@@ -4,9 +4,40 @@ import java.util.ArrayList;
 
 import edu.westga.cs6312.model.Wood;
 
+/**
+ * Defines GoWestLumber object
+ * @author justinmaxwell
+ * @version 2/6/23
+ *
+ */
 public class GoWestLumber {
 	private ArrayList<Wood> lumberInventory;
 	
+	/**
+	 * GoWestLumber constructor
+	 */
+	public GoWestLumber() {
+		this.setLumberInventory(new ArrayList<Wood>());
+	}
+	
+	/**
+	 * Getter
+	 * @return lumberInventory
+	 */
+	public ArrayList<Wood> getLumberInventory() {
+		return this.lumberInventory;
+	}
+	
+	/**
+	 * Setter
+	 * @param lumberInventory set lumberInventory
+	 */
+	public void setLumberInventory(ArrayList<Wood> lumberInventory) {
+		if (lumberInventory == null) {
+			throw new IllegalArgumentException("lumberInventory cannot be null");
+		}
+		this.lumberInventory = lumberInventory;
+	}
 	
 	/**
 	 * Returns the most expensive Wood in inventory
@@ -29,9 +60,8 @@ public class GoWestLumber {
 	 * @param woodNames the names to compare to
 	 * @return a list of Wood(s) -case agnostic - or empty list if none match
 	 */
-	public ArrayList<Wood> getWoodByNames(ArrayList<String> woodNames){
+	public ArrayList<Wood> getWoodByNames(ArrayList<String> woodNames) {
 		return null;
 	}
-	
 	
 }
