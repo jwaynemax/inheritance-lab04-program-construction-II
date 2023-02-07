9d4afb058f5aa9308038c6128a5ce9994b856c62
Justin Maxwell <jm00724@my.westga.edu>
2023-02-07 16:58:40 -0500

    Test data removed from Main

--- src/edu/westga/cs6312/main/Main.java
+++ src/edu/westga/cs6312/main/Main.java
@@ -1,14 +1,5 @@
 package edu.westga.cs6312.main;
 
-import java.util.ArrayList;
-
-import edu.westga.cs6312.controller.GoWestLumber;
-import edu.westga.cs6312.model.Abura;
-import edu.westga.cs6312.model.OrientedStrandBoard;
-import edu.westga.cs6312.model.Tamarind;
-import edu.westga.cs6312.model.Wood;
-import edu.westga.cs6312.model.YellowBirch;
-
 /**
  * define objects
  * @author justinmaxwell
@@ -22,27 +13,7 @@ public class Main {
 	 * @param args not used
 	 */
 	public static void main(String[] args) {
-		Abura myWood1 = new Abura();
-		Tamarind myWood2 = new Tamarind();
-		YellowBirch myWood3 = new YellowBirch();
 		
-		ArrayList<String> names = new ArrayList<String>();
-		names.add(myWood1.getScientificName());
-		names.add("Sample");
-		names.add(myWood3.getScientificName());
-		
-		ArrayList<Wood> lumberInventory = new ArrayList<Wood>();
-		lumberInventory.add(myWood1);
-		lumberInventory.add(myWood2);
-		lumberInventory.add(myWood3);
-		
-		GoWestLumber myStore = new GoWestLumber();
-		
-		myStore.setLumberInventory(lumberInventory);
-				
-		System.out.println(myStore.getWoodByNames(names));
-
-				
 	}
 
 }
